ecd67bff7f69b767c0f10671be067fc25c38281a
harry <harrytsao12345@gmail.com>
2024-03-08 21:30:04 -0500

    iniial auth framework with clerk and update file structure

--- zenstate/app/(root)/layout.tsx
+++ zenstate/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={'${inter.className} bg-slate-800 text-slate-100'}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={'${inter.className} bg-slate-800 text-slate-100'}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- zenstate/app/(root)/page.tsx
+++ zenstate/app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import Stopwatch from '@/components/Stopwatch'
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from '../../components/Navbar'
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
-  return <Navbar />;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
 }
 
 // const page = () => {
