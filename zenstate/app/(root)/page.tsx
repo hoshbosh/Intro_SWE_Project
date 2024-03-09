1fafb28186c1f353d7d4043c823c73aa0238f8a3
harry <harrytsao12345@gmail.com>
2024-03-09 14:56:49 -0500

    second commit on auth and landing page

--- zenstate/app/(root)/page.tsx
+++ zenstate/app/(root)/page.tsx
@@ -1,14 +1,15 @@
 import Stopwatch from '@/components/Stopwatch'
 import React from 'react'
-import Navbar from '../../components/Navbar'
 import { UserButton } from "@clerk/nextjs"
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/"/>
+      <Footer />
     </div>
-  )
+  ) 
 }
 
 // const page = () => {
