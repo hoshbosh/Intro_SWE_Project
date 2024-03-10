f06a770ab028494eed71dc2b003b49dc4f777fab
Kelly Yuan <115099372+kellyyuan333@users.noreply.github.com>
2024-03-09 21:15:36 -0500

    Update page.tsx

--- zenstate/app/(auth)/onboarding/page.tsx
+++ zenstate/app/(auth)/onboarding/page.tsx
@@ -1,9 +1,25 @@
-async function Page() {
-    return (
-        <main>
-            <h1 className="head-text">Onboarding</h1>
-        </main>
-    )
+import { UserButton } from "@clerk/nextjs";
+import Link from 'next/link';
+import NavbarUser from '@/components/NavbarUser'
+import Footer from '@/components/Footer'
+import { BackgroundGradientAnimation } from '@/components/ui/background-gradient-animation'
+
+export default function onboarding() {
+  return (
+    <div>
+        <NavbarUser />
+        <BackgroundGradientAnimation />
+        <Footer />
+    </div>
+  )
 }
 
-export default Page;
+// async function Page() {
+//     return (
+//         <main>
+//             <h1 className="head-text">Onboarding</h1>
+//         </main>
+//     )
+// }
+
+// export default Page;
