65b5e159385835f5accf44ecee135bd38d07a19b
Kelly Yuan <115099372+kellyyuan333@users.noreply.github.com>
2024-03-09 22:01:42 -0500

    Update Navbar.tsx

--- zenstate/components/Navbar.tsx
+++ zenstate/components/Navbar.tsx
@@ -9,16 +9,16 @@ const Navbar = () => {
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0">
-                <img
+                <a href="/"><img
                   src="/ZenState.svg"
                   alt="ZenState"
                   className="h-8"
-                />
+                /></a>
               </div>
             </div>
             <div className="hidden md:block">
               <div className="ml-4 flex items-center space-x-4">
-                <a href="/login" className="text-sm border border-slate-300 text-slate-300 px-4 
+                <a href="/onboarding" className="text-sm border border-slate-300 text-slate-300 px-4 
                 py-0.5 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">
                   Login
                 </a>
