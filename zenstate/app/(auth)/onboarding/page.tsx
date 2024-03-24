041c8fb2406b1877d393ef1f024803ff6c7087d0
XJXuan888 <115206157+XJXuan888@users.noreply.github.com>
2024-03-24 16:47:52 -0400

    Update onboarding page.tsx
    add stopwatch component

--- zenstate/app/(auth)/onboarding/page.tsx
+++ zenstate/app/(auth)/onboarding/page.tsx
@@ -1,15 +1,31 @@
 import { UserButton } from "@clerk/nextjs";
 import Link from 'next/link';
-import NavbarUser from '@/components/NavbarUser'
+import NavbarUser from '@/components/Navbar/NavbarUser'
 import Footer from '@/components/Footer'
 import { BackgroundGradientAnimation } from '@/components/ui/background-gradient-animation'
+import WeekForm from "@/components/weeklypoints/weeklypoints";
+import Leaderboard from "@/components/Leaderboard/Leaderboard";
+import Stopwatch from "@/components/timer/Stopwatch";
+
 
 export default function onboarding() {
   return (
     <div>
         <NavbarUser />
-        <BackgroundGradientAnimation />
-        <Footer />
+      <BackgroundGradientAnimation>
+      <div style={{ position: 'absolute', top: '5px', left: '50px', zIndex: 999 }}>
+          <Stopwatch />
+        </div>
+
+        <div style={{ position: 'absolute', bottom: '250px', left: '20px', zIndex: 999 }}>
+          <WeekForm />
+        </div>
+        <div style={{ position: 'absolute', bottom: '250px', right: '80px', zIndex: 999 }}>
+          <Leaderboard />
+        </div>
+       
+      </BackgroundGradientAnimation>
+      <Footer />
     </div>
   )
 }
